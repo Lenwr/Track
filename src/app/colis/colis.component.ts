cbf7e020b506588ca3eb834ad22e3e093512999c
Lenwr <saidabou.lateam@gmail.com>
2023-02-05 18:56:26 +0100

    mes outils

--- src/app/colis/colis.component.ts
+++ src/app/colis/colis.component.ts
@@ -8,17 +8,33 @@ import { UsersDataService } from '../service/users-data.service';
   styleUrls: ['./colis.component.css']
 })
 export class ColisComponent implements OnInit {
-  DATA: any
-  constructor(private myDataService: UsersDataService) { }
+  v1: any
+  v2: any
+  v3: any
+  punitaire:any
+  vol:any
+  constructor() { }
 
   ngOnInit(): void {
-    this.myDataService.users().subscribe((result: any) => {
-      this.DATA = result.data
-      console.log(this.DATA)
-    })
+
   }
 
-  displayedColumns: string[] = ['nom', 'prenom','addresse', 'telephone'];
+volume(){
+ let volume = document.getElementById("volume") as HTMLInputElement
+  let val1 = document.getElementById("valeur1") as HTMLInputElement
+  let val2 = document.getElementById("valeur2") as HTMLInputElement
+  let val3 = document.getElementById("valeur3") as HTMLInputElement
+  let pu = document.getElementById("pu") as HTMLInputElement
+
+  this.v1 = val1.value
+  this.v2 = val2.value
+  this.v3 = val3.value
+  this.punitaire = pu.value
+
+    this.vol = this.v1 * this.v2 * this.v3 * this.punitaire
+ volume.value = this.vol
 
 
+
+  }
 }
